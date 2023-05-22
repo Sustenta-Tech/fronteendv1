85696a8444948a7f6d52545c7def8b7556c4450d
Daisy Kelly Viana <daisyviana96@gmail.com>
2023-05-22 12:25:53 -0300

    Sobre

--- src/paginas/sobre/Sobre.tsx
+++ src/paginas/sobre/Sobre.tsx
@@ -1,14 +1,44 @@
-import {  Button, Grid, Typography } from "@material-ui/core";
+import { Button, Grid, Typography } from "@material-ui/core";
 import { Box } from "@mui/material";
 import React from "react";
 
 function Sobre() {
     return (
         <>
-             <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
                         <Typography variant="h4" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Sobre nós</Typography>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/3CytTdi.jpg" alt="" width="724px" height="500px" />
+                </Grid>
+            </Grid>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/3CytTdi.jpg" alt="" width="670px" height="500px" />
+                </Grid>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h4" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Missão e Valores</Typography>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h4" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Tecnologias</Typography>
                         <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
