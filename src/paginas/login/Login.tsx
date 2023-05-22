264c78583aca14c2796cad4f0370bec22b8fc043
Giselle Souza <reallovetour@gmail.com>
2023-05-22 19:09:41 -0300

    Estilização página de login

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -49,21 +49,22 @@ function Login() {
     }
 
     return (
-        <Grid container direction='row' justifyContent='center' alignItems='center'>
-            <Grid alignItems='center' xs={6}>
-                <Box paddingX={20} className='border'>
+        <Grid container direction='row' className='grid'>
+            
+            <Grid xs={6} container direction='row' justifyContent='flex-end'>
+                <Box className='cardInfo'>
                     <form onSubmit={onSubmit}>
 
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='tituloLogin'>
                             Entrar
                         </Typography>
 
-                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} id='usuario' label='email' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} id='usuario' label='E-mail' name='usuario' margin='normal' fullWidth />
 
-                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} id='senha' label='Senha' name='senha' margin='normal' type='password' fullWidth />
 
                         <Box marginTop={2} textAlign='center'>
-                            <Button type='submit' variant='contained' color='primary'>
+                            <Button type='submit' variant='contained' id='buttonEntrar' className='button'>
                                 Acessar
                             </Button>
                         </Box>
@@ -71,16 +72,16 @@ function Login() {
                 </Box>
             </Grid>
 
-            <Grid className='grid' alignItems='center' xs={6}>
-                <Box className='cardVerde'>
-                    <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>
-                        Bem Vindo de Volta
+            <Grid xs={6} container direction='row' justifyContent='flex-start'>
+                <Box paddingX={5} className='cardVerde'>
+                    <Typography variant='h3' gutterBottom component='h3' className='subtitulo'>
+                        Ainda não tem cadastro?
                     </Typography>
-                    <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'</Typography>
+                    <p className="texto">Se cadastre no link abaixo para conseguir efetuar as suas compras e aproveitar ainda mais o nosso site!</p>
 
                     <Box marginTop={2} textAlign='center'>
                         <Link to="/cadastro">
-                            <Button type='submit' variant='contained' color='primary'>
+                            <Button type='submit' variant='contained' className='button'>
                                 Cadastre-se
                             </Button>
                         </Link>
