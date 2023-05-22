2a70b8a78fe2ac1116740b6499ef67221bf702a2
Andresa Marçal <andresamarques3107@gmail.com>
2023-05-22 10:46:34 -0300

    Criação Navbar, Carrossel e Footer

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,11 +1,67 @@
-import React from "react";
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { Box } from '@mui/material'
+import './Navbar.css'
+import { Link } from 'react-router-dom';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Navbar() {
     return (
         <>
-           <h1>Navbar</h1>
+            <AppBar position='static' style={{ backgroundColor:"green" }}>
+                <Toolbar variant='dense'>
+                    <Box className='cursor'>
+                        <Typography variant='h5'>
+                            Sustenta Tech
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Link to='/home'className='text-decorator-none'>
+                            <Box mx={1}  className='cursor'>
+                                <Typography variant='h6'>
+                                    Home
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to='/sobre' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                        <Typography variant='h6'>
+                                Sobre
+                        </Typography>
+                        </Box>
+                        </Link>
+                        <Link to='/produtos' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                        <Typography variant='h6' >
+                           Produtos
+                        </Typography>
+                        </Box>
+                        </Link>
+                        <Link to='/contato' className='text-decorator-none'>
+                        <Box mx={1}  className='cursor'>
+                        <Typography variant='h6' >
+                            Contato
+                        </Typography>
+                        </Box>
+                        </Link>
+                        <Link to='/login' className='text-decorator-none'>
+                        <Box mx={1}  className='cursor'>
+                        <Typography variant='h6'>
+                            Login
+                        </Typography>
+                        </Box>
+                        </Link>
+                        <Link to='/carrinho' className='text-decorator-none'>
+                        <Box mx={1}  className='cursor'>
+                            <ShoppingCartIcon />
+                        </Box>
+                        </Link>
+                    </Box>
+                </Toolbar>
+            </AppBar>
         </>
     )
 }
 
-export default Navbar
+export default Navbar;
