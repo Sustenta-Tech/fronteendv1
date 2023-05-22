8df8af98d7966715d13e44c48ee1d0b8918ee123
Nicolle Sanches <nicollesanches.ri@outlook.com>
2023-05-22 12:55:05 -0300

    Criação Contato

--- src/paginas/contato/Contato.tsx
+++ src/paginas/contato/Contato.tsx
@@ -1,11 +1,46 @@
 import React from "react";
+import "./Contato.css"
 
 function Contato() {
     return (
         <>
-           <h1>Contato</h1>
+
+           
+
+                <main className="contato">
+                    <section className="formulario">
+                        <h3>Contato</h3>
+                        <form>
+                            <div className="campos-form">
+                                <label htmlFor="nome">Nome:</label>
+                                <input onKeyUp="validarNome()" id="nome" type="text" placeholder="Insira o seu nome completo..."/>
+                                    <div id="txtNome"></div>
+                            </div>
+                            <div className="campos-form">
+                                <label htmlFor="email">E-mail:</label>
+                                <input onKeyUp="validarEmailRegEx()" id="email" type="text" placeholder="Insira o seu e-mail..."/>
+                                    <div id="txtEmail"></div>
+                            </div>
+                            <div className="campos-forma">
+                                <label htmlFor="mensagem">Mensagem:</label>
+                                <textarea id="mensagem" type="text" rows="8" placeholder="Insira o sua mensagem..."></textarea>
+                                <div id="txtMensagem"></div>
+                            </div>
+                            <button onClick="enviarFormulario()" type="button">Enviar</button>
+                        </form>
+                    </section>
+
+
+                </main>
+
+                    
+                
+            
+
+
         </>
     )
+
 }
 
-export default Contato
+export default Contato;
