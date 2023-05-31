b4fab53899875601723b0d4027f0e262e16c9298
Andresa Marçal <andresamarques3107@gmail.com>
2023-05-30 23:50:09 -0300

    Update Sobre

--- src/paginas/sobre/Sobre.tsx
+++ src/paginas/sobre/Sobre.tsx
@@ -10,7 +10,7 @@ function Sobre() {
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
                         <Typography variant="h4" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Somos Sustenta Tech</Typography>
-                        <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Somos uma organização com o compromisso de oferecer alternativas inteligentes e profissionais para atender às necessidades de nossos clientes e do planeta.
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Somos uma organização que tem o compromisso de oferecer alternativas inteligentes e profissionais para atender às necessidades de nossos clientes e do planeta.
                             Nos dedicamos a encontrar soluções inovadoras e eficientes, que ajudem a impactar positivamente o mundo e melhorar a vida das pessoas.
                             Os nossos valores são guiados pela ética, qualidade e respeito ao meio ambiente.
                         </Typography>
@@ -21,18 +21,20 @@ function Sobre() {
                     </Box>
                 </Grid>
                 <Grid item xs={6} className="imagemlogo">
-                    <img src="src\imagens\logo.png" />
+                    <img src="src\imagens\iconesobre3.png"/>
                 </Grid>
                 {/* <Grid item xs={6} >
                     <img src="https://i.imgur.com/3CytTdi.jpg" alt="" width="724px" height="500px" referrerPolicy="no-referrer"/>
                 </Grid> */}
             </Grid>
-            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
-                <Grid item xs={6} className="imagem"></Grid>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='cardmissao' >
+                <Grid item xs={6} className="imagem">
+                <img src="src\imagens\iconesobre.png"/>
+                </Grid>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h4" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Nossa Missão</Typography>
-                        <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>A Sustenta Tech acredita que juntos, podemos promover um futuro mais sustentável e ecologicamente viável. Oferecemos soluções que visam incentivar a utilização de fontes renováveis de energia, garantindo produtos de alta qualidade e confiabilidade.</Typography>
+                        <Typography variant="h4" gutterBottom color="textPrimary" component="h3" align="center" className='devscard'>Nossa Missão</Typography>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" className='devscard'>A Sustenta Tech acredita que juntos, podemos promover um futuro mais sustentável e ecologicamente viável. Oferecemos soluções que visam incentivar a utilização de fontes renováveis de energia, garantindo produtos de alta qualidade e confiabilidade.</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
@@ -51,43 +53,89 @@ function Sobre() {
                         </Box>
                     </Box>
                 </Grid>
-                <Grid item xs={6} className="imagemlampada"></Grid>
+                <Grid item xs={6} className="imagemlampada">
+                <img src="src\imagens\iconesobre2.png"/>
+                </Grid>
             </Grid>
 
 {/* GRID DESENVOLVEDOR */}
 
-            <Grid container spacing={2}>
+            <Grid container spacing={2} className='cardtech'>
                 <Grid item xs={3}>
-                    <h1>pessoa1</h1>
-                    <Box paddingX={20} >
-                        <Typography variant="h4" gutterBottom color="textPrimary" component="h1" align="center" className='titulo'>Andresa Marçal</Typography>
-                        <Typography variant="h6" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Typography>
+                    <Box paddingX={5} >
+                        <h3>Andresa Marçal</h3>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h3" align="center" className='devscard'>Formada em Recursos Humanos e atualmente cursando Análise e Desenvolvimento de Sistemas. Sou apaixonada por desafios. Encontrei na Tecnologia a paixão que me impulsiona a aprender e evoluir constantemente.</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
                     </Box>
                 </Grid>
+
                 <Grid item xs={3}>
-                    <h1>pessoa2</h1>
+                    <Box paddingX={5} >
+                        <h3>Andressa Camillo</h3>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h3" align="center"  className='devscard'>Formada em Administração de Empresas e atualmente cursando Analise e Desenvolvimento de Programas. Sempre enxerguei a tecnologia como uma ferramenta poderosa para otimizar o trabalho e facilitar a vida das pessoas.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}> 
+                        </Box>
+                    </Box>
                 </Grid>
+
                 <Grid item xs={3}>
-                    <h1>pessoa3</h1>
+                <Box paddingX={5} >
+                    <h3>Daisy Viana</h3>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h3" align="center" className='devscard'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                    </Box>
                 </Grid>
+
                 <Grid item xs={3}>
-                    <h1>pessoa4</h1>
+                <Box paddingX={5} >
+                    <h3>Giselle Souza</h3>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h3" align="center" className='devscard'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                    </Box>
                 </Grid>
 
                 <Grid item xs={4}>
-                    <h1>pessoa5</h1>
+                <Box paddingX={5} >
+                    <h3>João Vitor Moreira</h3> 
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h3" align="center" className='devscard'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                    </Box>
                 </Grid>
 
                 <Grid item xs={4}>
-                    <h1>pessoa6</h1>
+                <Box paddingX={5} >
+                    <h3>Nicolle Sanches</h3>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h3" align="center" className='devscard'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                    </Box>
                 </Grid>
 
                 <Grid item xs={4}>
-                    <h1>pessoa7</h1>
+                <Box paddingX={5} >
+                    <h3>Yngrid Padilha</h3>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h3" align="center" className='devscard'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                    </Box>
                 </Grid>
             </Grid>
 
