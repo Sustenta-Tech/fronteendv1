c81fada9837291cfa888e0bdf813a675c45b590a
Nicolle Sanches <nicollesanches.ri@outlook.com>
2023-05-31 11:07:47 -0300

    Carrossel Informativo

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -3,6 +3,7 @@ import { Button, Grid, Typography } from '@material-ui/core'
 import { Box } from '@mui/material'
 import './Home.css'
 import Carrossel from "../../components/carrossel/Carrossel";
+import CarrosselInfo from "../../components/carrosselInfo/CarrosselInfo";
 import ListaProdutos from "../../components/produtos/listaproduto/ListaProdutos";
 // import ListaCategoria from "../../components/categorias/listacategoria/ListaCategoria";
 
@@ -14,6 +15,12 @@ function Home() {
                     <Carrossel />
                     {/* <ListaCategoria /> */}
                     <ListaProdutos />
+                </Grid>
+            </Grid>
+
+            <Grid container style={{ marginTop: "8px" }}>
+                <Grid item xs={12}>
+                    <CarrosselInfo />
                 </Grid>
             </Grid>
         </>
